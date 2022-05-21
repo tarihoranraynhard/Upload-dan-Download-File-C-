bab7238e3693813c9a83382e49b1c4d741fdbf89
Raynhard <99233356+tarihoranraynhard@users.noreply.github.com>
2022-05-21 13:28:42 +0700

    Add files via upload

---
+++ U&D_File/MainForm.Designer.cs
@@ -0,0 +1,227 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ASUS
+ * Date: 19/05/2022
+ * Time: 10:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace U_D_File
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.input_id = new System.Windows.Forms.TextBox();
+			this.btnOpenUpload = new System.Windows.Forms.Button();
+			this.btnUpload = new System.Windows.Forms.Button();
+			this.btnOpenDownload = new System.Windows.Forms.Button();
+			this.btnDownload = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label4 = new System.Windows.Forms.Label();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BackColor = System.Drawing.Color.Cyan;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(31, 56);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(282, 35);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Upload File";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.BackColor = System.Drawing.Color.Cyan;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(394, 56);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(295, 35);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Download File";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.White;
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox1.Location = new System.Drawing.Point(31, 94);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(282, 186);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.White;
+			this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox2.Location = new System.Drawing.Point(394, 94);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(295, 186);
+			this.pictureBox2.TabIndex = 3;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label3
+			// 
+			this.label3.BackColor = System.Drawing.Color.MediumSpringGreen;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(394, 283);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(140, 26);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Input id Gambar";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// input_id
+			// 
+			this.input_id.Location = new System.Drawing.Point(540, 283);
+			this.input_id.Name = "input_id";
+			this.input_id.Size = new System.Drawing.Size(149, 22);
+			this.input_id.TabIndex = 5;
+			// 
+			// btnOpenUpload
+			// 
+			this.btnOpenUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnOpenUpload.Location = new System.Drawing.Point(31, 342);
+			this.btnOpenUpload.Name = "btnOpenUpload";
+			this.btnOpenUpload.Size = new System.Drawing.Size(102, 34);
+			this.btnOpenUpload.TabIndex = 6;
+			this.btnOpenUpload.Text = "Open File";
+			this.btnOpenUpload.UseVisualStyleBackColor = true;
+			this.btnOpenUpload.Click += new System.EventHandler(this.BtnOpenUploadClick);
+			// 
+			// btnUpload
+			// 
+			this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnUpload.Location = new System.Drawing.Point(211, 342);
+			this.btnUpload.Name = "btnUpload";
+			this.btnUpload.Size = new System.Drawing.Size(102, 34);
+			this.btnUpload.TabIndex = 7;
+			this.btnUpload.Text = "Upload";
+			this.btnUpload.UseVisualStyleBackColor = true;
+			this.btnUpload.Click += new System.EventHandler(this.BtnUploadClick);
+			// 
+			// btnOpenDownload
+			// 
+			this.btnOpenDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnOpenDownload.Location = new System.Drawing.Point(394, 342);
+			this.btnOpenDownload.Name = "btnOpenDownload";
+			this.btnOpenDownload.Size = new System.Drawing.Size(102, 34);
+			this.btnOpenDownload.TabIndex = 8;
+			this.btnOpenDownload.Text = "Open File";
+			this.btnOpenDownload.UseVisualStyleBackColor = true;
+			this.btnOpenDownload.Click += new System.EventHandler(this.BtnOpenDownloadClick);
+			// 
+			// btnDownload
+			// 
+			this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDownload.Location = new System.Drawing.Point(587, 342);
+			this.btnDownload.Name = "btnDownload";
+			this.btnDownload.Size = new System.Drawing.Size(102, 34);
+			this.btnDownload.TabIndex = 9;
+			this.btnDownload.Text = "Download";
+			this.btnDownload.UseVisualStyleBackColor = true;
+			this.btnDownload.Click += new System.EventHandler(this.BtnDownloadClick);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 429);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(712, 170);
+			this.dataGridView1.TabIndex = 10;
+			// 
+			// label4
+			// 
+			this.label4.BackColor = System.Drawing.Color.DarkCyan;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(12, 396);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(712, 30);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Data Dari Database";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(736, 611);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.btnDownload);
+			this.Controls.Add(this.btnOpenDownload);
+			this.Controls.Add(this.btnUpload);
+			this.Controls.Add(this.btnOpenUpload);
+			this.Controls.Add(this.input_id);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "U&D_File";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button btnDownload;
+		private System.Windows.Forms.Button btnOpenDownload;
+		private System.Windows.Forms.Button btnUpload;
+		private System.Windows.Forms.Button btnOpenUpload;
+		private System.Windows.Forms.TextBox input_id;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ U&D_File/MainForm.cs
@@ -0,0 +1,168 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ASUS
+ * Date: 19/05/2022
+ * Time: 10:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Data.OleDb;
+using MySql.Data.MySqlClient;
+
+namespace U_D_File
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string connectionSQL = "server = localhost; database = ud_file; uid = root; password = ;";
+		Bitmap img;
+			string pathing;
+			string SourceFilePath;
+			string inputid;
+			int intinputid;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			ReadData();
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		
+		void BtnOpenUploadClick(object sender, EventArgs e)
+		{
+			if(DialogResult.OK == openFileDialog1.ShowDialog()){
+				img = new Bitmap(openFileDialog1.FileName);
+				pictureBox1.Image = img;
+				pathing = openFileDialog1.FileName.ToString();
+			}
+		}
+		
+		
+		
+		void BtnUploadClick(object sender, EventArgs e)
+		{
+			File25SqlBlob(pathing);
+			ReadData();
+		}
+		
+		private void File25SqlBlob(string SourceFilePath) {
+			try {
+				MySqlConnection koneksi = new MySqlConnection(connectionSQL);
+				MySqlCommand cmd = new MySqlCommand("INSERT into tablefile(id, DataFile) values ("+incrementValueId()+",?Picture)", koneksi);
+				System.IO.FileStream fs = new System.IO.FileStream(SourceFilePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+				Byte[] b = new Byte[fs.Length];
+				fs.Read(b, 0, b.Length);
+				fs.Close();
+				MySqlParameter P = new MySqlParameter("?Picture", MySqlDbType.LongBlob, b.Length, ParameterDirection.Input, false, 0, 0, null, DataRowVersion.Current, b);
+				cmd.Parameters.Add(P);
+				koneksi.Open();
+				if (cmd.ExecuteNonQuery() == 1)
+					MessageBox.Show("Gambar berhasil di simpan ke database");
+				
+				koneksi.Close();
+			}
+			catch(MySqlException ex) {
+				MessageBox.Show(ex.Message);
+			}
+			ReadData();
+		}
+		
+		
+		
+		void BtnOpenDownloadClick(object sender, EventArgs e)
+		{
+			try{
+				MySqlConnection koneksi = new MySqlConnection(connectionSQL);
+				koneksi.Open();
+				MySqlDataAdapter dataAdapter = new MySqlDataAdapter (new MySqlCommand("SELECT DataFile FROM tablefile WHERE id = "+input_id.Text, koneksi));
+				DataSet dataSet = new DataSet();
+				dataAdapter.Fill(dataSet);
+				
+				if (dataSet.Tables[0].Rows.Count == 1) {
+					Byte[] data = new Byte[0];
+					data = (Byte[])(dataSet.Tables[0].Rows[0]["DataFile"]);
+					MemoryStream men = new MemoryStream(data);
+					pictureBox2.Image = Image.FromStream(men);
+				}
+				koneksi.Close();
+			}
+			catch(Exception ex) {
+				MessageBox.Show(ex.Message);
+			}
+		}
+		
+		
+		
+		void BtnDownloadClick(object sender, EventArgs e)
+		{
+			DialogResult dr = saveFileDialog1.ShowDialog();
+			if (dr == DialogResult.OK) {
+				SaveFileDialog sfd = new SaveFileDialog();
+				sfd.Filter = "Image|.png;.bmp;*.jpg";
+				ImageFormat format = ImageFormat.Png;
+				if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+					string ext = System.IO.Path.GetExtension(sfd.FileName);
+					switch (ext) {
+						case ".jpg" :
+							format = ImageFormat.Jpeg;
+							break;
+						case ".bmp" :
+							format = ImageFormat.Bmp;
+							break;
+					}
+					pictureBox2.Image.Save(sfd.FileName, format);
+				}
+				    
+			}
+		}
+		
+		private int incrementValueId() {
+			int temp = 0;
+			try{
+				MySqlConnection db = new MySqlConnection(connectionSQL);
+				db.Open();
+				MySqlCommand dbcmd = db.CreateCommand();
+				string sql = "SELECT max(id) from tablefile";
+				dbcmd.CommandText = sql;
+				MySqlDataReader reader = dbcmd.ExecuteReader();
+				while (reader.Read()) {
+					if (!reader.IsDBNull(0)) {
+						return Convert.ToInt16(reader.GetString(0))+1;
+					}
+				}
+				return 0;
+			}
+			catch (MySqlException error) {
+				MessageBox.Show(error.ToString());
+				return 0;
+			}
+		}
+		
+		public void ReadData() {
+			MySqlConnection koneksi = new MySqlConnection(connectionSQL);
+			koneksi.Open();
+			DataTable dt = new DataTable();
+			MySqlDataAdapter da = new MySqlDataAdapter("SELECT * from tablefile", koneksi);
+			da.Fill(dt);
+			dataGridView1.DataSource = dt.DefaultView;
+			koneksi.Close();
+		}
+	}
+}
